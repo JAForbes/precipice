1d45aa9abf746256a366561c740fc4215f66cd3c
James <james.a.forbes@gmail.com>
2014-08-23 12:04:37 +1000

    Update mouse component

--- js/systems.js
+++ js/systems.js
@@ -5,8 +5,6 @@ Systems = {
 		E('Canvas').each(function(can,e){
 			can.el.width = can.width
 			can.el.height = can.height
-			can.con.translate(can.width/2,can.height/2)
-			can.con.fillRect(0,0,20,20)
 		});
 	}
 
