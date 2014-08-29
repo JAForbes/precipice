100d742d502eec810e05b1e4193efe6fb38ce499
James <james.a.forbes@gmail.com>
2014-08-29 14:20:19 +1000

    Preload sounds and images before playing game

--- js/game.js
+++ js/game.js
@@ -50,7 +50,7 @@ function loop () {
 	} else {
 		$('.villager_count').html('<p>Game will restart in 4 seconds</p>')
 		_.delay(function(){
-			location.reload()
+			//location.reload()
 
 		},4000)
 	}
@@ -70,88 +70,6 @@ window.onkeydown = function(e){
 }
 
 
-gameID = E({
-	Canvas: { el: can, width: 1, height: 1, con: can.getContext('2d') },
-	Mouse: { x: can.width/2, y: can.height/2, },
-	Paused: { paused: false },
-	Screen: { width: 0, height: 0},
-    Score: {},
-})
-
-
-
-home = E({
-    Position: {x: can.width/2, y:can.height/2},
-    Velocity: {x: 0,y:0},
-    LockPosition: {position: 'center'},
-    Strength: o({ strength: 150 }),
-    Arc: { radius: 150, ratio: 0, theta: { start:0 , end: 2*Math.PI} },
-    Frame: {scale:10 , playspeed: 1/5, frame: new Frame().reset(wormhole) },
-    GestureShoot: {},
-    DamageOnCollision: { inCircle: true },
-    FrameScale: { component: 'Strength', key: 'strength', multiplier: 0.07 },
-    StrengthLose: { min: 50},
-    ArcStrength: {}
-})
-
-defender = E({
-    Position: {x: can.width/2, y:can.height/2},
-    LockPosition: {position: 'center'},
-    Frame: {scale:3 , playspeed: 1/10, frame: new Frame().reset(wizard) },
-    State: {action:'idle'},
-    MouseState: { down: 'cast' },
-    Strength: E('Strength',home)
-})
-
-E('Strength',defender).change(function(){
-  var state = E('State',defender);
-  state.action = 'hurt';
-  _.delay(function(){
-    state.action = 'idle';
-  },200)
-})
-
-shield = E({
-    Arc : { radius: 0, ratio: 0, theta: { start: 0, end: 0} },
-    GestureShield: { protect: home },
-    Position: E('Position',home),
-    Strength: o({ strength: 0 }),
-    DamageOnCollision: { inCircle: true, inArc: true },
-})
-
-
-E({
-    SpawnClock: {
-
-        positions : function(){
-            return [
-                {x: 100, y: 100},
-                {x: can.width - 100, y: 100},
-                {x: 100, y: can.height - 100},
-                {x: can.width - 100, y: can.height - 100},
-                {x: 100 , y: can.height /2},
-                {x: can.width-100, y: can.height /2},
-
-            ]
-        },
-        clock: 100,
-        spawnRate: 100,
-
-    }
-})
-
-E({
-    SpawnVillagers: { clock: 900, rate: 1000 }
-})
-
-
-E({
-	Sound: { 
-		Remove: '#sounds #hits',
-		TargetReached: '#sounds #warp'
-	}
-})
-
 function updateScreen(){
     var $el = $(window);
     var w = $el.width()
@@ -162,8 +80,7 @@ function updateScreen(){
     setTimeout(drawTrees,200)
 }
 
-$(window).resize(updateScreen)
-updateScreen()
+
 
 
 function drawTrees() {
@@ -190,27 +107,114 @@ function drawTrees() {
     })
 }
 
-var mc = new Hammer.Manager(can, {});
+startGame = function(){
+    
+    var mc = new Hammer.Manager(can, {});
 
-var pan = new Hammer.Pan();
-mc.add(pan);
+    var pan = new Hammer.Pan();
+    mc.add(pan);
 
-mc.on('panstart', function(ev) {
-	var mouse = E('Mouse',gameID);
-    mouse.down = true;
-});
-mc.on('pan', function(ev) {
-	var p = ev.pointers[0];
-	var mouse = E('Mouse',gameID);
-	mouse.x = p.clientX
-    mouse.y = p.clientY
-    ev.preventDefault()
-});
-mc.on('panend', function(ev) {
-	//  transform.pos = ev.center;
-	var mouse = E('Mouse',gameID);
-    mouse.down = false;
+    mc.on('panstart', function(ev) {
+        var mouse = E('Mouse',gameID);
+        mouse.down = true;
+    });
+    mc.on('pan', function(ev) {
+        var p = ev.pointers[0];
+        var mouse = E('Mouse',gameID);
+        mouse.x = p.clientX
+        mouse.y = p.clientY
+        ev.preventDefault()
+    });
+    mc.on('panend', function(ev) {
+        //  transform.pos = ev.center;
+        var mouse = E('Mouse',gameID);
+        mouse.down = false;
 
-});
+    });
 
-loop()
+
+    gameID = E({
+        Canvas: { el: can, width: 1, height: 1, con: can.getContext('2d') },
+        Mouse: { x: can.width/2, y: can.height/2, },
+        Paused: { paused: false },
+        Screen: { width: 0, height: 0},
+        Score: {},
+    })
+
+
+
+    home = E({
+        Position: {x: can.width/2, y:can.height/2},
+        Velocity: {x: 0,y:0},
+        LockPosition: {position: 'center'},
+        Strength: o({ strength: 150 }),
+        Arc: { radius: 150, ratio: 0, theta: { start:0 , end: 2*Math.PI} },
+        Frame: {scale:10 , playspeed: 1/5, frame: new Frame().reset(wormhole) },
+        GestureShoot: {},
+        DamageOnCollision: { inCircle: true },
+        FrameScale: { component: 'Strength', key: 'strength', multiplier: 0.07 },
+        StrengthLose: { min: 50},
+        ArcStrength: {}
+    })
+
+    defender = E({
+        Position: {x: can.width/2, y:can.height/2},
+        LockPosition: {position: 'center'},
+        Frame: {scale:3 , playspeed: 1/10, frame: new Frame().reset(wizard) },
+        State: {action:'idle'},
+        MouseState: { down: 'cast' },
+        Strength: E('Strength',home)
+    })
+
+    E('Strength',defender).change(function(){
+      var state = E('State',defender);
+      state.action = 'hurt';
+      _.delay(function(){
+        state.action = 'idle';
+      },200)
+    })
+
+    shield = E({
+        Arc : { radius: 0, ratio: 0, theta: { start: 0, end: 0} },
+        GestureShield: { protect: home },
+        Position: E('Position',home),
+        Strength: o({ strength: 0 }),
+        DamageOnCollision: { inCircle: true, inArc: true },
+    })
+
+
+    E({
+        SpawnClock: {
+
+            positions : function(){
+                return [
+                    {x: 100, y: 100},
+                    {x: can.width - 100, y: 100},
+                    {x: 100, y: can.height - 100},
+                    {x: can.width - 100, y: can.height - 100},
+                    {x: 100 , y: can.height /2},
+                    {x: can.width-100, y: can.height /2},
+
+                ]
+            },
+            clock: 100,
+            spawnRate: 100,
+
+        }
+    })
+
+    E({
+        SpawnVillagers: { clock: 900, rate: 1000 }
+    })
+
+
+    E({
+        Sound: { 
+            Remove: '#hits',
+            TargetReached: '#warp'
+        }
+    })
+    updateScreen()
+    $(window).resize(updateScreen)
+    loop()
+}
